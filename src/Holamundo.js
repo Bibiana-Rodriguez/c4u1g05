50ac6772f715529dd5a92ce9e32a97f3c5813357
Alvaroarangel <111749183+Alvaroarangel@users.noreply.github.com>
2022-10-27 15:45:28 -0500

    app.js

---
+++ src/Holamundo.js
@@ -0,0 +1,14 @@
+// // function Holamundo() {
+// //     return (
+// //       <h1>Hola mundo </h1>
+// //     );
+// //   }
+
+//   const Holamundo= ()=>{
+//    return <h1>Hola Mundo</h1>
+  
+// }
+  
+//   export default Holamundo;
+// // export {Holamundo};
+  
