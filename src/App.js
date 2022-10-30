85ed45faae2ed0b5ccef852db5537e5fd7947bd8
Bibiana-Rodriguez <111749209+Bibiana-Rodriguez@users.noreply.github.com>
2022-10-29 20:56:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+var express = require("express");
+//npmvar bodyParser = require("body-parser");
+var app = express();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-              hola compañeros
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//var chocolate_router = require('./routers/Chocolate');
+//var mongoose = require('mongoose');
 
-export default App;
+
+app.use(express.json());
+app.use(express.urlencoded({
+extended: true
+}));
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, XRequested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+    });
+
+    app.use(require('../PROYECTO_NJS/routers/routes.js'))
+    module.exports = app;
